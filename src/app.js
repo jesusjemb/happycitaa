52deadb311b0c81264976ce5290df1677c1ecf99
jesusjemb <jesusemmauelmbjesuse@outlook.es>
2023-12-20 10:05:19 -0600

    update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-import express from "express"
-import {PORT} from "./config.js"
+// Importa las bibliotecas necesarias
+import express from 'express';
+import http from 'http';
 
-const app = express()
+// Crea una instancia de la aplicación Express
+const app = express();
+const port = 3000;
 
-app.get("/", (req,res)=>{
-	res.send("welcome XD")
-})
+// Middleware para servir archivos estáticos (como tu página HTML)
+app.use(express.static('public'));
 
-app.listen(PORT)
-console.log("Se usa ",PORT)
+// Ruta raíz que responde con "¡Hola, mundo!"
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo!');
+});
+
+// Crea un servidor HTTP y escucha en el puerto especificado
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`Servidor HTTP escuchando en http://localhost:${port}`);
+});
---
+++ src/ws.js
@@ -0,0 +1,78 @@
+import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
+import tmi from 'tmi.js';
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Configuración del cliente de Twitch
+const twitchClient = new tmi.Client({
+  options: { debug: true },
+  connection: {
+    secure: true,
+    reconnect: true,
+  },
+  channels: ['renrize'], // Reemplaza con tu nombre de canal
+});
+
+// Crear un servidor HTTP
+const server = http.createServer(app);
+
+// Crear un servidor WebSocket
+const wss = new WebSocket.Server({ server });
+
+// Manejar conexiones WebSocket
+wss.on('connection', (ws) => {
+  console.log('Conexión WebSocket establecida');
+
+  // Manejar mensajes del chat y enviarlos a través de WebSocket
+  twitchClient.on('message', (channel, userstate, message, self) => {
+    if (self) return; // Ignorar mensajes propios
+
+    const chatMessage = `${userstate.username}: ${message}`;
+    console.log(chatMessage);
+
+    // Enviar el mensaje a través de WebSocket a todos los clientes conectados
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        try {
+          client.send(chatMessage);
+          console.log('Mensaje enviado al WebSocket:', chatMessage);
+        } catch (error) {
+          console.error('Error al enviar mensaje al WebSocket:', error);
+        }
+      }
+    });
+  });
+
+  // Manejar errores en la conexión WebSocket
+  ws.on('error', (error) => {
+    console.error('Error en la conexión WebSocket:', error);
+  });
+
+  // También puedes manejar mensajes recibidos desde el front-end
+  ws.on('message', (message) => {
+    console.log(`Mensaje recibido desde el front-end: ${message}`);
+    // Puedes realizar acciones adicionales aquí según tus necesidades
+  });
+});
+
+// Manejar errores en la conexión del cliente de Twitch
+twitchClient.on('connected', (address, port) => {
+  console.log(`Conectado al servidor de Twitch en ${address}:${port}`);
+});
+
+twitchClient.on('disconnected', (reason) => {
+  console.error('Desconectado del servidor de Twitch:', reason);
+});
+
+// Iniciar el servidor
+server.listen(port, () => {
+  console.log(`Servidor de Chat de Twitch escuchando en http://localhost:${port}`);
+});
+
+// Conectar al chat de Twitch después de que el servidor se haya iniciado
+server.on('listening', () => {
+  twitchClient.connect();
+});
